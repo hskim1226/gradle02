81501b4d359b831d8f9ae44127eb6f6ed3323832
zbum <jibum.jung@gmail.com>
2014-08-24 18:51:00 +0900

    Q&A 기능 진행 중

--- src/main/java/com/apexsoft/ysprj/qna/web/QnaController.java
+++ src/main/java/com/apexsoft/ysprj/qna/web/QnaController.java
@@ -1,6 +1,12 @@
 package com.apexsoft.ysprj.qna.web;
 
+import com.apexsoft.framework.persistence.dao.page.PageInfo;
+import com.apexsoft.ysprj.qna.service.QnaService;
+import com.apexsoft.ysprj.qna.service.QnaVO;
+import com.apexsoft.ysprj.qna.web.form.QnaSearchForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,8 +21,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/qna")
 public class QnaController {
 
+    @Autowired
+    private QnaService qnaService;
+
     @RequestMapping(value="/list", method = RequestMethod.GET)
-    public String displayQnaList(){
+    public String displayQnaList(QnaSearchForm searchForm, Model model){
+        PageInfo<QnaVO> qnaVOPageInfo = qnaService.getQnaPaginatedList(searchForm);
+
+        model.addAttribute("qna", qnaVOPageInfo.getData());
+        model.addAttribute("qnaTotal", qnaVOPageInfo.getTotalRowCount());
 
         return "qna/list";
     }
---
+++ src/main/java/com/apexsoft/ysprj/qna/web/form/QnaSearchForm.java
@@ -0,0 +1,51 @@
+package com.apexsoft.ysprj.qna.web.form;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zbum
+ * Date: 2014. 8. 24.
+ * Time: 오후 6:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class QnaSearchForm {
+
+    private int pageNum = 1;
+
+    private int pageRows = 30;
+
+    private String searchKeyword;
+
+    private String searchCondition;
+
+    public String getSearchKeyword() {
+        return searchKeyword;
+    }
+
+    public void setSearchKeyword(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+    }
+
+    public String getSearchCondition() {
+        return searchCondition;
+    }
+
+    public void setSearchCondition(String searchCondition) {
+        this.searchCondition = searchCondition;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageRows() {
+        return pageRows;
+    }
+
+    public void setPageRows(int pageRows) {
+        this.pageRows = pageRows;
+    }
+}
