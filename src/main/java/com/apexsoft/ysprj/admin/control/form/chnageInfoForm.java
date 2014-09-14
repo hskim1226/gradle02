9d80d8cca1775f58be62b11b49c4ad8c61d2309d
dhoonkim <dhoonkim@apexsoft.co.kr>
2014-09-14 20:57:37 +0900

    '조회완성'

---
+++ src/main/java/com/apexsoft/ysprj/admin/control/form/chnageInfoForm.java
@@ -0,0 +1,36 @@
+package com.apexsoft.ysprj.admin.control.form;
+
+/**
+ * Created by DhKim on 2014-09-14.
+ */
+public class chnageInfoForm {
+
+    private String applNo = null;
+    private String afterItem = null;
+    private String infoRadio = null;
+
+
+    public String getApplNo() {
+        return applNo;
+    }
+
+    public void setApplNo(String applNo) {
+        this.applNo = applNo;
+    }
+
+    public String getAfterItem() {
+        return afterItem;
+    }
+
+    public void setAfterItem(String afterItem) {
+        this.afterItem = afterItem;
+    }
+
+    public String getInfoRadio() {
+        return infoRadio;
+    }
+
+    public void setInfoRadio(String infoRadio) {
+        this.infoRadio = infoRadio;
+    }
+}
