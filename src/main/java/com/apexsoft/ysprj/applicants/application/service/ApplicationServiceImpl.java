847244c873da4f641ecd46d8c8f11dce26a9b3bf
hanmomhanda <hanmomhanda@naver.com>
2014-09-18 14:09:03 +0900

    학력 update를 acadSeq 기준으로 건별 CUD 처리

--- src/main/java/com/apexsoft/ysprj/applicants/application/service/ApplicationServiceImpl.java
+++ src/main/java/com/apexsoft/ysprj/applicants/application/service/ApplicationServiceImpl.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Administrator on 2014-08-12.
@@ -173,20 +171,18 @@ public class ApplicationServiceImpl implements ApplicationService {
         acadList.addAll(graduateList);
 
         ExecutionContext ec = new ExecutionContext();
-        int r1 = 0, applNo = application.getApplNo(), idx = 0;
-        Date date = new Date();
+        int applNo = application.getApplNo();
+        ParamForAcademy param = new ParamForAcademy();
+        param.setApplNo(applNo);
+        param.setOrderBy("ACAD_SEQ");
 
-        deleteListByApplNo(applNo, "CustomApplicationAcademyMapper");
-        if ( acadList != null ) {
-            for( ApplicationAcademy academy : acadList) {
-                academy.setApplNo(applNo);
-                academy.setAcadSeq(++idx);
-                academy.setModDate(date);
-            }
-            r1 = commonDAO.insertList(acadList, NAME_SPACE, "ApplicationAcademyMapper");
-        }
+        param.setAcadTypeCode("00002");
+        int r1 = processAcademy(application, collegeList, applNo, new Date(), param);
 
-        if ( r1 > 0 ) {
+        param.setAcadTypeCode("00003");
+        int r2 = processAcademy(application, graduateList, applNo, new Date(), param);//
+
+        if ( r1 == collegeList.size() && r2 == graduateList.size() ) {
             ec.setResult(ExecutionContext.SUCCESS);
             ec.setMessage(messageResolver.getMessage("U317"));
             ec.setData(new CustomApplNoStsCode(applNo, application.getApplStsCode()));
@@ -198,6 +194,69 @@ public class ApplicationServiceImpl implements ApplicationService {
         }
         return ec;
     }
+
+    /**
+     * 현재 ACAD LIST 가져와서 HashMap을 만들고
+     * 화면에서 가져온 리스트 돌면서 HashMap 체크해서
+     * 케이스 별로 CRUD 처리
+     * 화면 seq 있고, DB seq 있으면 화면 레코드로 수정 하고 Map에서 삭제
+     * 화면 seq 있고, DB seq 없는 케이스는 없음
+     * 화면 seq 공백, DB seq 없으면 화면 레코드로 생성
+     * 화면 seq 공백, DB seq 있으면 Map에 남아있으므로 DB레코드 삭제 하고 Map에서 삭제
+     *
+     * @param application
+     * @param academyList
+     * @param applNo
+     * @param date
+     * @param param
+     * @return
+     */
+    private int processAcademy(Application application,
+                               List<ApplicationAcademy> academyList,
+                               int applNo,
+                               Date date,
+                               ParamForAcademy param) {
+
+        int c1 = 0, u1 = 0, d1 = 0;
+        List<ApplicationAcademy> academiesFromDB = commonDAO.queryForList(NAME_SPACE+"CustomApplicationAcademyMapper.selectByApplNoAcadTypeCode", param, ApplicationAcademy.class);
+        int lastSeq = academiesFromDB.get(academiesFromDB.size()-1).getAcadSeq();
+        Map<Integer, Integer> seqMap = new HashMap<Integer, Integer>();
+        if ( academiesFromDB != null ) {
+            for (ApplicationAcademy academy : academiesFromDB) {
+                seqMap.put(academy.getAcadSeq(), academy.getAcadSeq());
+            }
+        }
+        if ( academyList != null ) {
+            for( ApplicationAcademy academyFromView : academyList) {
+                int acadSeqFromView = academyFromView.getAcadSeq();
+                if ( acadSeqFromView > 0) { //화면 seq 있는 경우
+                    if ( seqMap.containsKey(acadSeqFromView) ) { //화면 seq 값이 DB에도 있는 경우
+                        //update
+                        academyFromView.setModId(application.getUserId());
+                        academyFromView.setModDate(date);
+                        u1 += commonDAO.updateItem(academyFromView, NAME_SPACE, "ApplicationAcademyMapper");
+                        seqMap.remove(acadSeqFromView);
+                    }
+                } else { // 화면 seq 가 숫자가 아닌 경우
+                    // 새 seq 부여해서 insert
+                    academyFromView.setApplNo(applNo);
+                    academyFromView.setAcadSeq(++lastSeq);
+                    academyFromView.setCreId(application.getUserId());
+                    academyFromView.setCreDate(date);
+                    c1 += commonDAO.insertItem(academyFromView, NAME_SPACE, "ApplicationAcademyMapper");
+                }
+            }
+            //Map에 남아있는 레코드는 delete
+            Set<Map.Entry<Integer, Integer>> entrySet = seqMap.entrySet();
+            for(Map.Entry<Integer, Integer> entry : entrySet) {
+                int seqFromDB = entry.getKey();
+                param.setAcadSeq(seqFromDB);
+                d1 += commonDAO.delete(NAME_SPACE + "CustomApplicationAcademyMapper.deleteByApplNoAcadTypeCodeAcadSeq", param);
+            }
+        }
+        return c1 + u1;
+    }
+
 
     @Override
     public ExecutionContext createLangCareer(Application application,
