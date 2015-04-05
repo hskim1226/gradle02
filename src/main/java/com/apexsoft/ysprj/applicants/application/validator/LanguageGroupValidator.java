9cea05970cbd2dfdf8de17d889608a6d1e43cab0
dhoonkim <dhoonkim@apexsoft.co.kr>
2015-04-05 20:53:06 +0900

    'document'

--- src/main/java/com/apexsoft/ysprj/applicants/application/validator/LanguageGroupValidator.java
+++ src/main/java/com/apexsoft/ysprj/applicants/application/validator/LanguageGroupValidator.java
@@ -62,19 +62,6 @@ public class LanguageGroupValidator extends NamedListValidator {
 
                     if (aLangOrExempt.isCheckedFg()) { // TOEFL, TOEIC, TEPS, ..., 면제사유
                         checkedCnt++;
-//                    if ("ENG_EXMP1".equals(langList.getSelGrpCode())) {
-//                        String selGrpCode = aLangOrExempt.getSelGrpCode();
-//                        if (selGrpCode == null || selGrpCode.length() == 0 || Integer.parseInt(selGrpCode) <= 0) {
-//                            errors.rejectValue(prefix + "selGrpCode", "U331",
-//                                    new Object[]{"외국어 시험 면제 사유"}, messageResolver.getMessage("U332"));
-//                        }
-//                    } else if ("KOR_EXMP1".equals(langList.getSelGrpCode())) {
-//                        String selGrpCode = aLangOrExempt.getSelGrpCode();
-//                        if (selGrpCode == null || selGrpCode.length() == 0 || Integer.parseInt(selGrpCode) <= 0) {
-//                            errors.rejectValue(prefix + "selGrpCode", "U331",
-//                                    new Object[]{"한국어 시험 면제 사유"}, messageResolver.getMessage("U332"));
-//                        }
-//                    }
                         if ("00002".equals(langList.getItemCode())) {
                             String subCode = aLangOrExempt.getSubCode();
                             if (subCode == null || subCode.length() == 0 || Integer.parseInt(subCode) <= 0) {
@@ -87,8 +74,6 @@ public class LanguageGroupValidator extends NamedListValidator {
                             String itemGrpCode = aLangOrExempt.getItemGrpCode();
                             String itemCode = aLangOrExempt.getItemCode();
                             if ("LANG_EXAM".equals(itemGrpCode) && "00001".equals(itemCode)) { // 시험이 TOEFL 일 경우 시험 종류 선택 필수
-//                        if ("00001".equals(itemCode)) { // 시험이 TOEFL 일 경우 시험 종류 선택 필수
-//                            if (aLangOrExempt.getToflTypeCode() == null || aLangOrExempt.getToflTypeCode().length() == 0) {
                                 if (aLangOrExempt.getSubCode() == null || aLangOrExempt.getSubCode().length() == 0) {
                                     errors.rejectValue(prefix + "subCode", "U331",
                                             new Object[]{messageResolver.getMessage("L03108")}, messageResolver.getMessage("U332"));  /*"TOEFL 시험 종류"*/
