d82c5e1f73ed7d7443e4982ea2209654f2861fc0
onetouch <onetouch@apexsoft.co.kr>
2015-09-18 16:11:51 +0900

    파일업로드 시 MultipartHttpServletRequest로 캐스팅 해서 처리하도록 리시버 수정

--- src/main/java/com/apexsoft/framework/persistence/file/receiver/MultiPartReceiverImpl.java
+++ src/main/java/com/apexsoft/framework/persistence/file/receiver/MultiPartReceiverImpl.java
@@ -44,47 +44,11 @@ public class MultiPartReceiverImpl implements MultiPartReceiver {
 
 	private MultiPartInfo receiveInternal(HttpServletRequest request) throws Exception {
 
-//		ServletFileUpload upload = new ServletFileUpload();
-//
-//		FileItemIterator iter = upload.getItemIterator(request);
-//
-//		Map<String, Object> attributes = new HashMap<String, Object>();
-//		List<FileItem> files = new ArrayList<FileItem>();
-//
-//		while (iter.hasNext()) {
-//			FileItemStream itemStream = iter.next();
-//
-//			if (itemStream.isFormField()) {
-//				handleAttribute(attributes, itemStream);
-//			} else {
-//				File f = File.createTempFile("upload", "temp");
-//				f.deleteOnExit();
-//
-//				OutputStream fos = new FileOutputStream(f);
-//				IOUtils.copyLarge(itemStream.openStream(), fos);
-//
-//				files.add(new FileItem(itemStream.getFieldName(), itemStream.getName(), f));
-//			}
-//		}
-//
-//		String queryString = request.getQueryString();
-//
-//		if(StringUtils.hasText(queryString)){
-//			attributes.putAll(getQueryParameters(queryString));
-//		}
 		DefaultMultipartHttpServletRequest defaultMultipartHttpServletRequest = (DefaultMultipartHttpServletRequest) request;
 		MultiValueMap<String, MultipartFile> fileMap = defaultMultipartHttpServletRequest.getMultiFileMap();
 		Map<String, String[]> paramMap = defaultMultipartHttpServletRequest.getParameterMap();
 
-//		DiskFileItemFactory factory = new DiskFileItemFactory();
-
-//		ServletFileUpload upload = new ServletFileUpload(factory);
-
-//		FileItemIterator iter = upload.getItemIterator(request);
-
-//		List<org.apache.commons.fileupload.FileItem> items = upload.parseRequest(defaultMultipartHttpServletRequest);
-
-		Map<String, Object> attributes = new HashMap<String, Object>();
+		Map<String, Object> attributes = getQueryParams(paramMap);
 		List<FileItem> files = new ArrayList<FileItem>();
 
 		Set<Map.Entry<String, List<MultipartFile>>> entrySet = fileMap.entrySet();
@@ -96,75 +60,78 @@ public class MultiPartReceiverImpl implements MultiPartReceiver {
 			OutputStream fos = new FileOutputStream(f);
 			List<MultipartFile> fileList = entry.getValue();
 			for (MultipartFile file : fileList) {
+                org.apache.commons.fileupload.FileItem fileItem = ((CommonsMultipartFile) file).getFileItem();
 				IOUtils.copyLarge(file.getInputStream(), fos);
-				files.add(new FileItem(entry.getKey(), file.getName(), f));
+				files.add(new FileItem(entry.getKey(), fileItem.getName(), f));
 			}
 		}
-		attributes.putAll(paramMap);
-//		String queryString = request.getQueryString();
-//
-//		if(StringUtils.hasText(queryString)){
-////			attributes.putAll(getQueryParameters(queryString));
-//			attributes.putAll(paramMap);
-//		}
 		
 		return new MultiPartInfo(attributes, files);
 	}
-	
-	private void handleAttribute(Map<String, Object> attributes, FileItemStream item) throws IOException {
-		Object currentValue = null;
 
-		String name = item.getFieldName();
-		String value = Streams.asString(item.openStream());
+    private Map<String, Object> getQueryParams(Map<String, String[]> paramMap) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        Set<Map.Entry<String, String[]>> entrySet = paramMap.entrySet();
+        for (Map.Entry<String, String[]> entry : entrySet) {
+            map.put(entry.getKey(), entry.getValue()[0]);
+        }
+        return map;
+    }
 
-		if (attributes.containsKey(name)) {
-			if (String[].class.isAssignableFrom(attributes.get(name).getClass())) {
-				currentValue = (String[]) attributes.get(name);
-
-			} else {
-				currentValue = (String[]) ArrayUtils.add((String[]) currentValue, attributes.get(name));
-			}
-
-			attributes.put(name, (String[]) ArrayUtils.add((String[]) currentValue, value));
-
-		} else {
-			attributes.put(name, value);
-		}
-	}
-	
-	private Map<String, Object> getQueryParameters(String queryString) {
-		Map<String, Object> map = new HashMap<String, Object>();
-
-		if(StringUtils.hasText(queryString)){
-			try {
-				//한글 decode추가
-				queryString = URLDecoder.decode(queryString.toString(), "utf-8");
-				String[] paramValues = queryString.split("&");
-
-				for (int i = 0; i < paramValues.length; ++i) {
-					String[] paramValue = paramValues[i].split("=", 2);
-
-					if(map.containsKey(paramValue[0])){
-						Object fromParamValue = map.get(paramValue[0]);
-						String[] toParamValue = null;
-
-						if(String[].class.isAssignableFrom(fromParamValue.getClass())){
-							toParamValue = (String[]) ArrayUtils.add((Object[]) fromParamValue, paramValue[1]);
-						} else {
-							toParamValue = (String[]) ArrayUtils.add(new String[]{(String)fromParamValue}, paramValue[1]);
-						}
-
-						map.put(paramValue[0], toParamValue);
-					} else {
-						map.put(paramValue[0], paramValue[1]);
-					}
-				}
-			} catch (UnsupportedEncodingException e1) {
-				throw new RuntimeException(e1);
-			}
-		}
-
-		return map;
-	}
+//	private void handleAttribute(Map<String, Object> attributes, FileItemStream item) throws IOException {
+//		Object currentValue = null;
+//
+//		String name = item.getFieldName();
+//		String value = Streams.asString(item.openStream());
+//
+//		if (attributes.containsKey(name)) {
+//			if (String[].class.isAssignableFrom(attributes.get(name).getClass())) {
+//				currentValue = (String[]) attributes.get(name);
+//
+//			} else {
+//				currentValue = (String[]) ArrayUtils.add((String[]) currentValue, attributes.get(name));
+//			}
+//
+//			attributes.put(name, (String[]) ArrayUtils.add((String[]) currentValue, value));
+//
+//		} else {
+//			attributes.put(name, value);
+//		}
+//	}
+//
+//	private Map<String, Object> getQueryParameters(String queryString) {
+//		Map<String, Object> map = new HashMap<String, Object>();
+//
+//		if(StringUtils.hasText(queryString)){
+//			try {
+//				//한글 decode추가
+//				queryString = URLDecoder.decode(queryString.toString(), "utf-8");
+//				String[] paramValues = queryString.split("&");
+//
+//				for (int i = 0; i < paramValues.length; ++i) {
+//					String[] paramValue = paramValues[i].split("=", 2);
+//
+//					if(map.containsKey(paramValue[0])){
+//						Object fromParamValue = map.get(paramValue[0]);
+//						String[] toParamValue = null;
+//
+//						if(String[].class.isAssignableFrom(fromParamValue.getClass())){
+//							toParamValue = (String[]) ArrayUtils.add((Object[]) fromParamValue, paramValue[1]);
+//						} else {
+//							toParamValue = (String[]) ArrayUtils.add(new String[]{(String)fromParamValue}, paramValue[1]);
+//						}
+//
+//						map.put(paramValue[0], toParamValue);
+//					} else {
+//						map.put(paramValue[0], paramValue[1]);
+//					}
+//				}
+//			} catch (UnsupportedEncodingException e1) {
+//				throw new RuntimeException(e1);
+//			}
+//		}
+//
+//		return map;
+//	}
 
 }
