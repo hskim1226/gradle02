420351cecd0612c8dff98c5388d22e728e4583bb
onetouch <onetouch@apexsoft.co.kr>
2015-12-21 18:15:16 +0900

    위변조 방지 PDF 넘버링 테스트

---
+++ src/main/java/com/apexsoft/framework/print/PDFPrinter.java
@@ -0,0 +1,66 @@
+package com.apexsoft.framework.print;
+
+import org.apache.pdfbox.pdfparser.PDFParser;
+
+import javax.print.*;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Sides;
+import javax.print.event.PrintJobAdapter;
+import javax.print.event.PrintJobEvent;
+import java.awt.print.PrinterJob;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * Created by hanmomhanda on 15. 12. 17.
+ */
+public class PDFPrinter {
+
+    private static boolean jobRunning = true;
+
+    private static class JobCompleteMonitor extends PrintJobAdapter {
+        @Override
+        public void printJobCompleted(PrintJobEvent pje) {
+            System.out.println("Job Completed");
+            jobRunning = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        InputStream is = new FileInputStream("/home/hanmomhanda/gitRepo/ysproject/src/main/java/com/apexsoft/framework/print/data.pdf");
+//        DocFlavor docFlavor = DocFlavor.INPUT_STREAM.PDF;
+        DocFlavor docFlavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE;
+        PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
+        aset.add(Sides.ONE_SIDED);
+
+
+        PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+        System.out.println("print service : " + printService.getName());
+
+
+
+//        PrintService[] printServices = PrintServiceLookup.lookupPrintServices(docFlavor, aset);
+//        if (printServices.length == 0){
+//            throw new IllegalStateException("No Printer Found");
+//        }
+        PrinterJob job = PrinterJob.getPrinterJob();
+//        job.setPrintService(printServices[0]);
+        job.setPrintService(printService);
+
+        PDFParser pdfParser = new PDFParser(is);
+        pdfParser.parse();
+        job.setPageable(pdfParser.getPDDocument());
+        job.print();
+
+//        DocPrintJob docPrintJob = printService.createPrintJob();
+//        docPrintJob.addPrintJobListener(new JobCompleteMonitor());
+//        Doc doc = new SimpleDoc(is, docFlavor, null);
+//        docPrintJob.print(doc, null);
+//        while(jobRunning){
+//            Thread.sleep(1000);
+//        }
+//        System.out.println("Terminated");
+        is.close();
+    }
+}
