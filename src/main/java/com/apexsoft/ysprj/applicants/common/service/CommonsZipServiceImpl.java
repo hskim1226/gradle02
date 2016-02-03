bf94a8333b8fde93060e76501b649dcdfcaabaf0
onetouch <onetouch@apexsoft.co.kr>
2016-02-03 12:40:57 +0900

    zip 압축 코드 리팩터링

--- src/main/java/com/apexsoft/ysprj/applicants/common/service/CommonsZipServiceImpl.java
+++ src/main/java/com/apexsoft/ysprj/applicants/common/service/CommonsZipServiceImpl.java
@@ -20,78 +20,67 @@ public class CommonsZipServiceImpl implements ZipService {
 
         File zippedFile = new File(targetDir, zipFileName);
 
-        OutputStream os = null;
+        ZipArchiveOutputStream zos = addFilesToZipArchive(fileList, zippedFile);
 
-        try {
-            os = new FileOutputStream(zippedFile);
-        } catch (FileNotFoundException e) {}
-
-        ZipArchiveOutputStream zos = new ZipArchiveOutputStream(os);
-        zos.setEncoding("UTF-8");
-        zos.setUseLanguageEncodingFlag(true);
-        zos.setFallbackToUTF8(true);
-        zos.setLevel(Deflater.NO_COMPRESSION);
-        ZipArchiveEntry ze;
-        int length;
-        byte[] buf = new byte[8 * 1024];
-        FileInputStream fis = null;
-
-        // 새 zip 파일에 파일 추가
-        for (File file : fileList) {
-            try {
-                String fileName = file.getName();
-                ze = new ZipArchiveEntry(fileName);
-                zos.putArchiveEntry(ze);
-                fis = new FileInputStream(file);
-                while ((length = fis.read(buf, 0, buf.length)) >= 0) {
-                    zos.write(buf, 0, length);
-                }
-                fis.close();
-                zos.closeArchiveEntry();
-            } finally {
-                if (fis != null)
-                    try { fis.close(); } catch (IOException e) {}
-            }
-        }
-
-        try {
-            zos.close();
-        } finally {
-            if (zos != null)
-                try { zos.close(); } catch (IOException e) {}
-        }
+//        OutputStream os = null;
+//
+//        try {
+//            os = new FileOutputStream(zippedFile);
+//        } catch (FileNotFoundException e) {}
+//
+//        ZipArchiveOutputStream zos = new ZipArchiveOutputStream(os);
+//        zos.setEncoding("UTF-8");
+//        zos.setUseLanguageEncodingFlag(true);
+//        zos.setFallbackToUTF8(true);
+//        zos.setLevel(Deflater.NO_COMPRESSION);
+//        ZipArchiveEntry ze;
+//        int length;
+//        byte[] buf = new byte[8 * 1024];
+//        FileInputStream fis = null;
+//
+//        // 새 zip 파일에 파일 추가
+//        for (File file : fileList) {
+//            try {
+//                String fileName = file.getName();
+//                ze = new ZipArchiveEntry(fileName);
+//                zos.putArchiveEntry(ze);
+//                fis = new FileInputStream(file);
+//                while ((length = fis.read(buf, 0, buf.length)) >= 0) {
+//                    zos.write(buf, 0, length);
+//                }
+//                fis.close();
+//                zos.closeArchiveEntry();
+//            } finally {
+//                if (fis != null)
+//                    try { fis.close(); } catch (IOException e) {}
+//            }
+//        }
+//
+        closeZipArchiveOutputStream(zos);
         return zippedFile;
     }
 
     @Override
     public File appendFilesToZipFile(List<File> fileList, File zipFile) throws IOException, InterruptedException {
-        OutputStream os = null;
+
         String zipFilePath = zipFile.getAbsolutePath();
         String zipFileDir = zipFilePath.substring(0, zipFilePath.lastIndexOf("/"));
         File tempZipFile = new File(zipFileDir, "temp.zip");
         FileUtils.copyFile(zipFile, tempZipFile);
         zipFile.delete();
 
-        try {
-            os = new FileOutputStream(new File(zipFilePath));
-        } catch (FileNotFoundException e) {}
+        File newZipFile = new File(zipFilePath);
 
-        ZipArchiveOutputStream zos = new ZipArchiveOutputStream(os);
-        zos.setEncoding("UTF-8");
-        zos.setUseLanguageEncodingFlag(true);
-        zos.setFallbackToUTF8(true);
-        zos.setLevel(Deflater.NO_COMPRESSION);
-        ZipArchiveEntry ze;
-        int length;
-        byte[] buf = new byte[8 * 1024];
-        FileInputStream fis = null;
+        ZipArchiveOutputStream zos = addFilesToZipArchive(fileList, newZipFile);
 
         // 기존 zip 파일에 있던 파일을 새 zip 파일에 추가
         ZipFile zFile = new ZipFile(tempZipFile);
         Enumeration<ZipArchiveEntry> enumZe = zFile.getEntries();
+        int length;
+        byte[] buf = new byte[8 * 1024];
         InputStream is = null;
         while(enumZe.hasMoreElements()) {
-            ze = enumZe.nextElement();
+            ZipArchiveEntry ze = enumZe.nextElement();
             try {
                 is = zFile.getInputStream(ze);
                 zos.putArchiveEntry(ze);
@@ -105,7 +94,30 @@ public class CommonsZipServiceImpl implements ZipService {
             }
         }
 
-        // 추가할 새 파일을 새 zip 파일에 추가
+        closeZipArchiveOutputStream(zos);
+
+        if (tempZipFile.exists()) tempZipFile.delete();
+
+        return zipFile;
+    }
+
+    private ZipArchiveOutputStream addFilesToZipArchive(List<File> fileList, File zipFile) throws IOException {
+        OutputStream os = null;
+        try {
+            os = new FileOutputStream(zipFile);
+        } catch (FileNotFoundException e) {}
+
+        ZipArchiveOutputStream zos = new ZipArchiveOutputStream(os);
+        zos.setEncoding("UTF-8");
+        zos.setUseLanguageEncodingFlag(true);
+        zos.setFallbackToUTF8(true);
+        zos.setLevel(Deflater.NO_COMPRESSION);
+
+        ZipArchiveEntry ze;
+        int length;
+        byte[] buf = new byte[8 * 1024];
+        FileInputStream fis = null;
+
         for (File file : fileList) {
             try {
                 String fileName = file.getName();
@@ -123,15 +135,15 @@ public class CommonsZipServiceImpl implements ZipService {
             }
         }
 
+        return zos;
+    }
+
+    private void closeZipArchiveOutputStream(ZipArchiveOutputStream zos) throws IOException {
         try {
             zos.close();
         } finally {
             if (zos != null)
                 try { zos.close(); } catch (IOException e) {}
         }
-
-        if (tempZipFile.exists()) tempZipFile.delete();
-
-        return zipFile;
     }
 }
