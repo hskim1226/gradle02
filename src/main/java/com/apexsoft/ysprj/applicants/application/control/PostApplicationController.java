1ca147f9259d70337342e3efe8d11b8b1c4ee99c
onetouch <onetouch@apexsoft.co.kr>
2016-02-05 20:36:48 +0900

    내원서에서 묶음 파일 다운로드 할 수 있도록 수정

--- src/main/java/com/apexsoft/ysprj/applicants/application/control/PostApplicationController.java
+++ src/main/java/com/apexsoft/ysprj/applicants/application/control/PostApplicationController.java
@@ -1,13 +1,9 @@
 package com.apexsoft.ysprj.applicants.application.control;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3Object;
 import com.apexsoft.ysprj.applicants.application.domain.Application;
+import com.apexsoft.ysprj.applicants.application.service.DocumentService;
 import com.apexsoft.ysprj.applicants.common.domain.BirtRequest;
-import com.apexsoft.ysprj.applicants.common.util.FilePathUtil;
-import org.apache.commons.io.IOUtils;
+import com.apexsoft.ysprj.applicants.common.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -16,13 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URLEncoder;
-import java.security.Principal;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by hanmomhanda on 15. 7. 13.
@@ -41,7 +36,7 @@ public class PostApplicationController {
     private String s3MidPath;
 
     @Autowired
-    private AmazonS3Client s3Client;
+    private DocumentService documentService;
 
     /**
      * 신청 완료된 원서나 수험표를 S3에서 다운로드
@@ -50,70 +45,31 @@ public class PostApplicationController {
      */
     @RequestMapping(value = "/download/{docType}", method = RequestMethod.POST, produces = "application/pdf")
     @ResponseBody
-    public byte[] fileDownload(@PathVariable("docType") String docType, BirtRequest birtRequest,
-//   public void fileDownload(@PathVariable("docType") String docType, BirtRequest birtRequest,
-                               Principal principal,
-                               HttpServletRequest request,
+    public byte[] fileDownload(@PathVariable("docType") String type, BirtRequest birtRequest,
                                HttpServletResponse response)
-            throws IOException {
+            throws IOException, InterruptedException {
 
-        Application application = birtRequest.getApplication();
-        String admsNo = application.getAdmsNo();
-//        String userId = principal.getName();
-        String userId = application.getUserId();
-        int applNo = application.getApplNo();
+        int applNo = birtRequest.getApplication().getApplNo();
+        Application application = documentService.getApplication(applNo);
 
-        String uploadDirPath = FilePathUtil.getUploadDirectoryFullPath(fileBaseDir, s3MidPath, admsNo, userId, applNo);
+        Map<String, byte[]> downloadableFileInfo = documentService.getDownloadableFileAsBytes(application, type);
+        Set<String> key = downloadableFileInfo.keySet();
+        Iterator<String> iter = key.iterator();
         String fileName = null;
-
-        switch(docType) {
-            case "appl":
-                fileName = FilePathUtil.getApplicationFormFileName(userId);
-                break;
-            case "adms":
-                fileName = FilePathUtil.getApplicationSlipFileName(userId);
-                break;
-            case "merged":
-                fileName = FilePathUtil.getFinalMergedFileName(applNo);
-                break;
+        if (iter.hasNext()) {
+            fileName = iter.next();
         }
+        byte[] bytes = downloadableFileInfo.get(fileName);
+        String downaloadFileName = StringUtil.urlEncodeSpecialCharacter(URLEncoder.encode(fileName, "UTF-8"));
 
-        String fileFullPath = uploadDirPath + "/" + fileName;
-
-        ServletContext sc = request.getSession().getServletContext();
-        String mimeType = sc.getMimeType(fileName);
-        if (mimeType == null) {
-            mimeType = "application/octet-stream";
-        }
-
-        S3Object object = s3Client.getObject(new GetObjectRequest(bucketName, FilePathUtil.getS3PathFromLocalFullPath(fileFullPath, fileBaseDir)));
-        InputStream inputStream = object.getObjectContent();
-        ObjectMetadata objMeta = object.getObjectMetadata();
-        byte[] bytes = IOUtils.toByteArray(inputStream);
-
-        request.setAttribute("Accept", "application/pdf");
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + URLEncoder.encode(fileName, "UTF-8") + "\"");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + downaloadFileName + "\"");
         response.setHeader("Content-Transfer-Encoding", "binary;");
         response.setHeader("Pragma", "no-cache;");
         response.setHeader("Expires", "-1;");
-        response.setHeader("Content-Type", mimeType);
+        response.setHeader("Content-Type", "application/octet-stream");
         response.setContentLength(bytes.length);
 
         return bytes;
-
-
-
-//        OutputStream outputStream = response.getOutputStream();
-//        IOUtils.copy(new ByteArrayInputStream(bytes), outputStream);
-//        response.setHeader("Content-Disposition", "attachment; filename=\"" + URLEncoder.encode(fileName, "UTF-8") + "\"");
-//        response.setHeader("Content-Transfer-Encoding", "binary;");
-//        response.setHeader("Pragma", "no-cache;");
-//        response.setHeader("Expires", "-1;");
-//        response.setHeader("Content-Type", mimeType);
-//        response.setContentLength(bytes.length);
-//        response.flushBuffer();
-//        inputStream.close();
-//        outputStream.close();
     }
 
 }
