d1cd0c6241b385e8e25e157c703a8c07aeb35d90
hskim1226 <hskim@apexsoft.co.kr>
2017-01-06 19:13:24 +0900

    m,n으로 github에서 변경

--- src/main/java/day/day02.java
+++ src/main/java/day/day02.java
@@ -4,6 +4,6 @@ package day;
  * Created by hskim@apexsoft.co.kr on 2017-01-06.
  */
 public class day02 {
-    public int sub(int a, int b){return a-b;}
+    public int sub(int m, int n){return m-n;}
 
 }
