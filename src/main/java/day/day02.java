bfd8eb757192eaf78e04d340aded86c30a467018
김효숙 <hskim@apexsoft.co.kr>
2017-01-06 19:07:38 +0900

    덧셈

---
+++ src/main/java/day/day02.java
@@ -0,0 +1,9 @@
+package day;
+
+/**
+ * Created by hskim@apexsoft.co.kr on 2017-01-06.
+ */
+public class day02 {
+    public int sub(int a, int b){return a-b;}
+
+}
---
+++ src/test/java/day/day02Test.java
@@ -0,0 +1,20 @@
+package day;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by hskim@apexsoft.co.kr on 2017-01-06.
+ */
+public class day02Test {
+    @Test
+    public void subtest() {
+        day02 day =new day02();
+
+        assertThat(day.sub(3,1),is(equalTo(2)));
+
+    }
+}
